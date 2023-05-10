1c92b250e471973ebc800304fbe3a623b123e73a
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-05-09 23:11:33 -0400

    Update final_simulation.R

--- projects/tb/final_simulation.R
+++ projects/tb/final_simulation.R
@@ -7,8 +7,10 @@ library(bit64)
 library(parallel)
 library(smallDB)
 library(delaySim)
+library(furrr)
 
 source("github/delay_diagnosis/build_scripts/R/functions/simulation_functions.R")
+source("github/delay_diagnosis/build_scripts/R/functions/trend_functions.R")
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -28,19 +30,6 @@ if (!dir.exists(sim_out_path)) {
   dir.create(sim_out_path)
 }
 
-
-########################################
-###### Apply Filtering Criteria ########
-########################################
-
-# Apply any criteria to restrict to final study population here
-
-
-######################
-#### Overall Fits ####
-######################
-
-
 #### Load Data  ----------------------------------------------------------------
 
 load(paste0(sim_in_path,"all_dx_visits.RData"))
@@ -53,18 +42,32 @@ ssd_codes <- codeBuildr::load_ssd_codes(cond_name) %>%
          dx_ver = ifelse(type=="icd9",9L,10L)) %>%
   select(dx,dx_ver)
 
-
+# extract patient ids and number of patients
 patient_ids <- tm %>% 
   distinct(enrolid)
 
 n_patients <- nrow(patient_ids)
 
-#### Compute visit counts ------------------------------------------------------
+
+########################################
+###### Apply Filtering Criteria ########
+########################################
+
+# Apply any criteria to restrict to final study population here
+
+
+#####################################
+#### Compute Overall Trends/Fits ####
+#####################################
+
+# Here we compute the overall trends and number of misses outside of the bootstrap
+
+
+### Compute visit counts ------------------------------------------------------
 sim_tm_all <- all_dx_visits %>% 
   mutate(period = -days_since_index) %>%
   distinct(enrolid,period,days_since_index) %>%
-  inner_join(sim_obs,by = c("enrolid", "days_since_index")) %>% 
-  rename(patient_id=enrolid)
+  inner_join(sim_obs,by = c("enrolid", "days_since_index")) 
 
 all_vis_count <- sim_tm_all %>% 
   count(.,period) %>%
@@ -75,8 +78,7 @@ sim_tm_ssd <- all_dx_visits %>%
   mutate(period = -days_since_index) %>%
   inner_join(ssd_codes,by = c("dx", "dx_ver")) %>% 
   distinct(enrolid,period,days_since_index) %>%
-  inner_join(sim_obs,by = c("enrolid", "days_since_index")) %>% 
-  rename(patient_id=enrolid)
+  inner_join(sim_obs,by = c("enrolid", "days_since_index")) 
 
 ssd_vis_count <- sim_tm_ssd %>% 
   count(.,period) %>%
@@ -96,6 +98,7 @@ ssd_vis_count <- return_fits(ssd_vis_count,
                              cp = delay_params$cp,
                              periodicity = delay_params$periodicity)
 
+
 all_vis_count %>% summarise(n_miss = sum(num_miss,na.rm = T))
 ssd_vis_count %>% summarise(n_miss = sum(num_miss,na.rm = T))
 
@@ -106,11 +109,12 @@ ssd_vis_count %>% summarise(n_miss = sum(num_miss,na.rm = T))
 
 #### Setup outer bootstrap data ------------------------------------------------
 
+# sample overall set of patients
 tmp <- tibble(boot_trial=1:delay_params$boot_trials) %>% 
   mutate(boot_sample = map(boot_trial,~tibble(enrolid = sample(patient_ids$enrolid, replace = TRUE)))) %>% 
   mutate(boot_sample = map(boot_sample, ~mutate(.,boot_id = row_number())))
 
-# compute counts for all visits
+#### compute counts for all visits ---------------------------------------------
 tmp <- tmp %>% 
   mutate(sim_tm = map(boot_sample,
                       ~inner_join(.,all_dx_visits, by = "enrolid") %>%
@@ -125,7 +129,7 @@ tmp <- tmp %>%
                                filter(period>0) %>% 
                                mutate(dow = as.factor(period %% 7))))
 
-# compute counts for ssd visits
+#### compute counts for ssd visits ---------------------------------------------
 tmp <- tmp %>% 
   mutate(sim_tm = map(boot_sample,
                       ~inner_join(.,all_dx_visits, by = "enrolid") %>%
@@ -141,11 +145,9 @@ tmp <- tmp %>%
                                filter(period>0) %>% 
                                mutate(dow = as.factor(period %% 7))))
 
+# remove the sim_tm data
 tmp <- tmp %>% 
   select(boot_trial,boot_sample,all_vis_count,ssd_vis_count)
-
-tmp$all_vis_count[[1]]
-tmp$ssd_vis_count[[1]]
 
 
 ### fit models -----------------------------------------------------------------
@@ -162,16 +164,25 @@ tmp <- tmp %>%
 # compute number missed
 tmp <- tmp %>% 
   mutate(n_miss_all = map(all_vis_count,
-                          ~summarise(.,n_miss_all = sum(num_miss,na.rm = T)))) %>% 
+                          ~summarise(.,n_miss_all = sum(round(num_miss,0),na.rm = T)))) %>% 
   unnest(n_miss_all) %>% 
   mutate(n_miss_ssd = map(ssd_vis_count,
-                          ~summarise(.,n_miss_ssd = sum(num_miss,na.rm = T)))) %>% 
+                          ~summarise(.,n_miss_ssd = sum(round(num_miss,0),na.rm = T)))) %>% 
   unnest(n_miss_ssd)
 
 
-tmp %>% 
-  summarise(n_miss_all = mean(n_miss_all),
-            n_miss_ssd = mean(n_miss_ssd))
+boot_data <- tmp
+rm(tmp)
+
+# save bootstrap data
+save(boot_data,file = paste0(sim_out_path,"boot_data.RData"))
+
+#### Compute number missed -----------------------------------------------------
+boot_data %>% 
+  summarise(mean_n_miss_all = mean(n_miss_all),
+            median_n_miss_all = median(n_miss_all),
+            mean_n_miss_ssd = mean(n_miss_ssd),
+            median_n_miss_ssd = median(n_miss_ssd))
 
 
 
@@ -179,150 +190,212 @@ tmp %>%
 #### Run bootstrap analysis ####
 ################################
 
-runs_sim_draw_visits()
 
-sim_tm_all
+cluster <- parallel::makeCluster(33)
 
-select(tmp$all_vis_count[[1]],period,num_miss)
+parallel::clusterCall(cluster, function() library(tidyverse))
+parallel::clusterCall(cluster, function() library(delaySim))
+parallel::clusterExport(cluster,c("run_boot_trials","boot_data","sim_tm_all","sim_tm_ssd","delay_params","n_patients"),
+                        envir=environment())
 
 
-tmp_sim_data <- list(time_map = mutate(sim_tm_all,miss_ind=1),
-                     miss_bins_visits = select(tmp$all_vis_count[[1]],period,num_miss),
-                     change_point = delay_params$cp,
-                     total_patients = n_patients,
-                     miss_bins = delay_params$miss_bins,
-                     duration_bins = delay_params$duration_bins)
+sim_res_all <- parallel::parLapply(cl = cluster,
+                               1:delay_params$boot_trials,
+                               function(x){run_boot_trials(boot_ids = boot_data$boot_sample[[x]],
+                                                           tm_data = sim_tm_all,
+                                                           miss_bins = select(boot_data$all_vis_count[[x]],period,num_miss),
+                                                           delay_params = delay_params,
+                                                           n_patients = n_patients)})
 
-run_sim_draw_simple(tmp_sim_data)
+sim_res_all <- map2(sim_res_all,1:delay_params$boot_trials,~mutate(.x,boot_trial=.y)) %>% 
+  bind_rows()
 
-mutate(sim_tm_all,miss_ind=1) %>% 
-  dplyr::filter(miss_ind == 1) %>% 
-  dplyr::inner_join(select(tmp$all_vis_count[[1]],period,num_miss), by = "period") %>% 
-  dplyr::mutate(rand = runif(n = dplyr::n())) %>% 
-  dplyr::arrange(period, rand) %>% 
-  dplyr::group_by(period) %>% 
-  dplyr::filter(dplyr::row_number() <= num_miss) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(-num_miss, -rand)
+sim_res_ssd <- parallel::parLapply(cl = cluster,
+                                   1:delay_params$boot_trials,
+                                   function(x){run_boot_trials(boot_ids = boot_data$boot_sample[[x]],
+                                                               tm_data = sim_tm_ssd,
+                                                               miss_bins = select(boot_data$ssd_vis_count[[x]],period,num_miss),
+                                                               delay_params = delay_params,
+                                                               n_patients = n_patients)})
 
-select(tmp$all_vis_count[[1]],period,num_miss) %>% summarise(sum(num_miss))
-tmp
+sim_res_ssd <- map2(sim_res_ssd,1:delay_params$boot_trials,~mutate(.x,boot_trial=.y)) %>% 
+  bind_rows()
 
-run_sim_draw_visits <- function(sim_data,overall_tm){
-  tmp <- run_sim_draw_simple(sim_data)
+parallel::stopCluster(cluster)
+
+rm(cluster)
+
+save(sim_res_all,file = paste0(sim_out_path,"sim_res_all.RData"))
+save(sim_res_ssd,file = paste0(sim_out_path,"sim_res_ssd.RData"))
+
+# now extract the specific sim obs values for future steps
+tmp1 <- sim_res_ssd %>% unnest(res) %>% distinct(obs)
+tmp2 <- sim_res_all %>% unnest(res) %>% distinct(obs)
+
+sim_obs_reduced <- sim_obs %>% 
+  inner_join(distinct(bind_rows(tmp1,tmp2))) %>% 
+  mutate(period = -days_since_index) %>% 
+  select(obs,enrolid,period)
+
+save(sim_obs_reduced, file = paste0(sim_out_path,"sim_obs_reduced.RData"))
+
+# cleanup
+rm(list = ls()[!(ls() %in% c("delay_params","sim_out_path","sim_in_path","cond_name"))])
+gc()
+
+###################################
+#### Analyze Bootstrap Results ####
+###################################
+
+# Load in sumulation results
+load(paste0(sim_out_path,"sim_res_ssd.RData"))
+load(paste0(sim_out_path,"sim_res_all.RData"))
+load(paste0(sim_out_path,"sim_obs_reduced.RData"))
+
+sim_res_ssd$res[[1]] %>% 
+  inner_join(sim_obs_reduced) %>% 
+  group_by(enrolid,boot_id) %>% 
+  summarise(duration=max(period),
+            n_miss=n())
+
+options(dplyr.summarise.inform = FALSE)
+
+compute_boot_stats <- function(sim_data,sim_obs_data,delay_params,n_patients){
   
-  
-  res1 <- distinct(tmp,obs)
-  
-  tmp2 <- tmp %>%
-    group_by(patient_id) %>%
+  tmp1 <- sim_data %>% 
+    inner_join(sim_obs_data, by = "obs") %>% 
+    group_by(enrolid,boot_id) %>%
     summarise(duration=max(period),
               n_miss=n())
   
   count_duration <- function(val){
-    tmp2 %>%
+    tmp1 %>%
+      ungroup() %>% 
       summarise(n=sum(duration>=val))
   }
   
-  res2 <- tmp2  %>%
+  res1 <- tmp1  %>%
+    ungroup() %>% 
     summarise(n_pat = n(),
               mean_dur = mean(duration),
               median_dur = median(duration),
               mean_n_miss = mean(n_miss),
               median_n_miss = median(n_miss))
   
-  res3 <- tibble(duration_bin = sim_data$duration_bins) %>%
+  res2 <- tibble(duration_bin = delay_params$duration_bins) %>%
     mutate(res = map(duration_bin,count_duration)) %>%
     unnest(res) %>%
-    mutate(pct_miss = 100*n/nrow(tmp2),
-           pct_all = 100*n/sim_data$total_patients)
-  
-  
+    mutate(pct_miss = 100*n/nrow(tmp1),
+           pct_all = 100*n/n_patients)
+
   count_miss <- function(val){
-    tmp2 %>%
+    tmp1 %>%
+      ungroup() %>% 
       summarise(n = sum(n_miss>=val))
   }
-  
-  res4 <- tibble(miss_bin = sim_data$miss_bins) %>%
+
+  res3 <- tibble(miss_bin = delay_params$miss_bins) %>%
     mutate(res = map(miss_bin,count_miss)) %>%
     unnest(res) %>%
-    mutate(pct_miss = 100*n/nrow(tmp2),
-           pct_all = 100*n/sim_data$total_patients)
+    mutate(pct_miss = 100*n/nrow(tmp1),
+           pct_all = 100*n/n_patients)
   
-  return(list(obs = res1,
-              stats = res2,
-              dur_tab = res3,
-              miss_tab = res4))
-}
-
-
-
-
-
-
-return_fits <- function(data, model, cp, periodicity = TRUE){
-  
-  if (periodicity==TRUE){
-    
-    if (model == "linear"){
-      
-      fit <- lm(n ~ period + dow, filter(data, period>cp))
-      
-    } else if (model == "exponential") {
-      
-      fit <- lm(n ~ log(period) + dow, filter(data, period>cp))
-      
-    } else if (model == "quadratic") {
-      
-      fit <- lm(n ~ poly(period,degree = 2) + dow, filter(data, period>cp))
-      
-    } else if (model == "cubic") {
-      
-      fit <- lm(n ~ poly(period,degree = 3) + dow, filter(data, period>cp))
-      
-    }
-    
-  } else {
-    
-    if (model == "linear"){
-      
-      fit <- lm(n ~ period, filter(data, period>cp))
-      
-    } else if (model == "exponential") {
-      
-      fit <- lm(n ~ log(period), filter(data, period>cp))
-      
-    } else if (model == "quadratic") {
-      
-      fit <- lm(n ~ poly(period,degree = 2), filter(data, period>cp))
-      
-    } else if (model == "cubic") {
-      
-      fit <- lm(n ~ poly(period,degree = 3), filter(data, period>cp))
-      
-    }
-    
-  }
-  
-  data %>%
-    mutate(pred = predict(fit,newdata = .)) %>% 
-    mutate(num_miss = n-pred) %>%
-    mutate(num_miss = ifelse(num_miss<0,0,num_miss)) %>%
-    mutate(num_miss = ifelse(period>cp,NA,num_miss))
+  return(list(stats = res1,
+              dur_tab = res2,
+              miss_tab = res3))
   
 }
-  
 
-#### fit models ----------------------------------------------------------------
-if (delay_params$periodicity){
-  # if periodicity = TRUE add day of week term
-  fit1 <- lm(n ~ period + dow, filter(all_vis_count, period>delay_params$cp))
-  fit2 <- lm(n ~ log(period) + dow, filter(all_vis_count, period>delay_params$cp))
-  fit3 <- lm(n ~ poly(period,degree = 2) + dow, filter(all_vis_count, period>delay_params$cp))
-  fit4 <- lm(n ~ poly(period,degree = 3) + dow, filter(all_vis_count, period>delay_params$cp))
-} else {
-  fit1 <- lm(n ~ period, filter(all_vis_count, period>delay_params$cp))
-  fit2 <- lm(n ~ log(period), filter(all_vis_count, period>delay_params$cp))
-  fit3 <- lm(n ~ poly(period,degree = 2), filter(all_vis_count, period>delay_params$cp))
-  fit4 <- lm(n ~ poly(period,degree = 3), filter(all_vis_count, period>delay_params$cp))
+compute_boot_stats(sim_res_ssd$res[[1]], sim_obs_reduced, delay_params = delay_params, n_patients = n_patients)
+
+
+
+plan(multisession, workers = 30)
+
+tmp1 <- sim_res_ssd %>% 
+  # slice(1:30) %>% 
+  mutate(sim_stats = future_map(res, ~compute_boot_stats(.,
+                                                         sim_obs_reduced,
+                                                         delay_params = delay_params, 
+                                                         n_patients = n_patients)))
+
+tmp1 <- tmp1 %>% 
+  select(sim_trial,boot_trial,sim_stats) %>% 
+  mutate(stats = map(sim_stats,~.$stats)) %>% 
+  mutate(miss_tab = map(sim_stats,~.$miss_tab)) %>% 
+  mutate(dur_tab = map(sim_stats,~.$dur_tab))
+
+sim_stats_ssd <- tmp1 %>% 
+  select(-sim_stats)
+
+
+tmp2 <- sim_res_all %>% 
+  # slice(1:30) %>% 
+  mutate(sim_stats = future_map(res, ~compute_boot_stats(.,
+                                                         sim_obs_reduced,
+                                                         delay_params = delay_params, 
+                                                         n_patients = n_patients)))
+
+tmp2 <- tmp2 %>% 
+  select(sim_trial,boot_trial,sim_stats) %>% 
+  mutate(stats = map(sim_stats,~.$stats)) %>% 
+  mutate(miss_tab = map(sim_stats,~.$miss_tab)) %>% 
+  mutate(dur_tab = map(sim_stats,~.$dur_tab))
+
+sim_stats_all <- tmp2 %>% 
+  select(-sim_stats)
+
+save(sim_stats_ssd,sim_stats_all,file = paste0(sim_out_path,"sim_stats.RData"))
+
+
+########################
+#### Export Results ####
+########################
+
+aggregate_sim_stats <- function(sim_stats_data){
+  tmp1 <- sim_stats_data %>% 
+    select(sim_trial,boot_trial,stats) %>% 
+    unnest(stats) %>% 
+    summarise_at(vars(n_pat:median_n_miss),~round(mean(.),2))
+  
+  tmp2 <- sim_stats_data %>% 
+    select(sim_trial,boot_trial,stats) %>% 
+    unnest(stats) %>% 
+    summarise_at(vars(n_pat:median_n_miss),~round(quantile(.,probs = c(0.025)),2))
+  
+  tmp3 <- sim_stats_data %>% 
+    select(sim_trial,boot_trial,stats) %>% 
+    unnest(stats) %>% 
+    summarise_at(vars(n_pat:median_n_miss),~round(quantile(.,probs = c(0.975)),2))
+  
+  
+  res1 <- inner_join(gather(tmp1, key=measure, value = mean),
+             gather(tmp2, key=measure, value = low)) %>% 
+    inner_join(gather(tmp3, key=measure, value = high)) %>% 
+    mutate(measure_out=paste0(mean," (",low,"-",high,")"))
+  
+  return(list(main_stats = res1))
 }
+
+aggregate_sim_stats(sim_stats_ssd)
+aggregate_sim_stats(sim_stats_all)
+
+
+sim_stats_ssd %>% 
+  select(sim_trial,boot_trial,miss_tab) %>% 
+  unnest(miss_tab) %>% 
+  group_by(miss_bin) %>% 
+  summarise_at(vars(n:pct_all),~round(mean(.),2))
+
+
+sim_stats_ssd %>% 
+  select(sim_trial,boot_trial,miss_tab) %>% 
+  unnest(miss_tab) %>% 
+  group_by(miss_bin) %>% 
+  summarise_at(vars(n:pct_all),~round(quantile(.,probs = c(0.025)),2))
+
+sim_stats_ssd %>% 
+  select(sim_trial,boot_trial,miss_tab) %>% 
+  unnest(miss_tab) %>% 
+  group_by(miss_bin) %>% 
+  summarise_at(vars(n:pct_all),~round(quantile(.,probs = c(0.975)),2))
