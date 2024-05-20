279614a4d2c08522094c7e08b57674bd1405d94b
aarmiller <34134737+aarmiller@users.noreply.github.com>
2024-05-20 15:30:08 -0500

    add job for delay duration models

--- misc/abx_delay_grant/run_duration_models.R
+++ misc/abx_delay_grant/run_duration_models.R
@@ -15,6 +15,13 @@ delay_base_path <- paste0("/Shared/Statepi_Diagnosis/prelim_results/",cond_name,
 load("/Shared/AML/params/delay_any_params.RData")
 # load("/Volumes/AML/params/delay_any_params.RData")
 
+
+out_path <- paste0(delay_base_path,"risk_models/")
+
+if (!dir.exists(out_path)) {
+  dir.create(out_path)
+}
+
 delay_params <- delay_any_params[[cond_name]]
 rm(delay_any_params)
 
@@ -265,19 +272,9 @@ mod_res2_2 <- mod_res2_2 %>%
 #### Categories of antibiotic duration ####
 ###########################################
 
+### Functions ------------------------------------------------------------------
 
-mod_res %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-
-## binning duration ------------------------------------------------------------
-
-
+# using max duration of antibiotic recieved during individual diagnostic opportunity window
 run_dur_model3 <- function(trial_num){
   
   tmp <- filter(sim_dur,trial==trial_num) %>% 
@@ -285,158 +282,18 @@ run_dur_model3 <- function(trial_num){
   
   tmp_abx_days <- abx_days %>% 
     inner_join(tmp, by = join_by(patient_id)) %>% 
-    filter(rx_date<index_date,
-           rx_date>=(index_date-duration)) %>% 
-    filter(abx_dates<index_date) %>% 
-    distinct(patient_id,abx_dates) %>% 
-    count(patient_id,name = "abx_days") 
-  
-  tmp_reg_data <- tmp %>% 
-    select(patient_id,duration) %>% 
-    inner_join(reg_data_base,by = join_by(patient_id)) %>% 
-    left_join(tmp_abx_days,by = join_by(patient_id)) %>% 
-    mutate(abx_days=replace_na(abx_days,0L))  %>% 
-    mutate(abx_days_cat = cut(abx_days,breaks = c(-1,0,3,5,7,10,99), labels = c("0","1-3","4-5","6-7","8-10",">10"))) 
-  
-  
-  tmp_fit <- lm(duration~age_cat+sex+abx_days_cat, data = tmp_reg_data) 
-  
-  out <- coef(tmp_fit)
-  
-  return(out)
-}
-
-rm(trial_num,tmp,out,tmp_reg_data,tmp_fit,tmp_abx_days)
-
-mod_res3 <- distinct(sim_res,trial)
-
-mod_res3 <- mod_res3 %>% 
-  mutate(res = map(trial,run_dur_model3))
-
-mod_res %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-mod_res2 %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-mod_res3 %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-
-######################################################
-#### Models That Don't Restrict Based on Duration ####
-######################################################
-
-filter_abx_days <- abx_days %>% 
-  inner_join(select(reg_data_base,patient_id,index_date)) %>% 
-  filter(rx_date<index_date & rx_date>(index_date-29)) %>% 
-  filter(abx_dates<index_date)
-
-trial_num <- 1
-
-run_dur_model1_2 <- function(trial_num){
-  
-  tmp <- filter(sim_dur,trial==trial_num)
-  
-  tmp_inds <- filter_abx_days %>%
-    distinct(patient_id) %>% 
-    mutate(abx_vis=1L) %>% 
-    inner_join(tmp,by = join_by(patient_id)) %>% 
-    distinct(patient_id,abx_vis)
-  
-  tmp_reg_data <- tmp %>% 
-    inner_join(reg_data_base,by = join_by(patient_id)) %>% 
-    left_join(tmp_inds,by = join_by(patient_id)) %>% 
-    mutate(abx_vis=replace_na(abx_vis,0L)) 
-  
-  
-  tmp_fit <- lm(duration~age_cat+sex+abx_vis, data = tmp_reg_data) 
-  
-  out <- coef(tmp_fit)
-  
-  return(out)
-}
-
-mod_res1_2 <- distinct(sim_res,trial) %>% 
-  mutate(res = map(trial,run_dur_model1_2))
-
-mod_res1_2 %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-filter_abx_days_count <- filter_abx_days %>% 
-  distinct(patient_id,abx_dates) %>% 
-  count(patient_id)
-
-run_dur_model2_2 <- function(trial_num){
-  
-  tmp <- filter(sim_dur,trial==trial_num) 
-  
-  
-  tmp_abx_days <- filter_abx_days_count %>% 
-    inner_join(tmp, by = join_by(patient_id)) %>% 
-    distinct(patient_id,abx_days=n) 
-  
-  tmp_reg_data <- tmp %>% 
-    select(patient_id,duration) %>% 
-    inner_join(reg_data_base,by = join_by(patient_id)) %>% 
-    left_join(tmp_abx_days,by = join_by(patient_id)) %>% 
-    mutate(abx_days=replace_na(abx_days,0L)) 
-  
-  
-  tmp_fit <- lm(duration~age_cat+sex+abx_days, data = tmp_reg_data) 
-  
-  out <- coef(tmp_fit)
-  
-  return(out)
-}
-
-rm(trial_num,tmp,out,tmp_reg_data,tmp_fit,tmp_abx_days)
-
-mod_res2_2 <- distinct(sim_res,trial) %>% 
-  mutate(res = map(trial,run_dur_model2_2))
-
-mod_res2_2 %>% 
-  mutate(res = map(res,enframe)) %>% 
-  unnest(res) %>% 
-  group_by(name) %>% 
-  summarise(mn_est = mean(value),
-            lo_est = quantile(value,probs = c(0.025)),
-            hi_est = quantile(value,probs = c(0.975)))
-
-run_dur_model3_2 <- function(trial_num){
-  
-  tmp <- filter(sim_dur,trial==trial_num) 
-  
-  tmp_abx_days <- filter_abx_days_count %>% 
-    inner_join(tmp, by = join_by(patient_id)) %>% 
-    distinct(patient_id,abx_days=n) 
+    filter(date<index_date,
+           date>=(index_date-duration)) %>% 
+    group_by(patient_id) %>% 
+    summarise(daysupp = max(daysupp)) %>% 
+    rename(abx_days=daysupp)
   
   tmp_reg_data <- tmp %>% 
     select(patient_id,duration) %>% 
     inner_join(reg_data_base,by = join_by(patient_id)) %>% 
     left_join(tmp_abx_days,by = join_by(patient_id)) %>% 
     mutate(abx_days=replace_na(abx_days,0L)) %>% 
-    mutate(abx_days_cat = cut(abx_days,breaks = c(-1,0,3,5,7,10,99), labels = c("0","1-3","4-5","6-7","8-10",">10"))) 
+    mutate(abx_days_cat = cut(abx_days,breaks = c(-1,0,3,5,7,10,14,99), labels = c("0","1-3","4-5","6-7","8-10","11-14",">14"))) 
   
   
   tmp_fit <- lm(duration~age_cat+sex+abx_days_cat, data = tmp_reg_data) 
@@ -445,17 +302,91 @@ run_dur_model3_2 <- function(trial_num){
   
   return(out)
 }
+# run_dur_model3(4)
 
-mod_res3_2 <- distinct(sim_res,trial) %>% 
+# using max duration of antibiotic recieved during individual diagnostic opportunity window
+run_dur_model3_2 <- function(trial_num){
+  
+  tmp <- filter(sim_dur,trial==trial_num) %>% 
+    inner_join(select(reg_data_base,patient_id,index_date),by = join_by(patient_id))
+  
+  tmp_abx_days <- abx_days %>% 
+    inner_join(tmp, by = join_by(patient_id)) %>% 
+    group_by(patient_id) %>% 
+    summarise(daysupp = max(daysupp)) %>% 
+    rename(abx_days=daysupp) 
+  
+  tmp_reg_data <- tmp %>% 
+    select(patient_id,duration) %>% 
+    inner_join(reg_data_base,by = join_by(patient_id)) %>% 
+    left_join(tmp_abx_days,by = join_by(patient_id)) %>% 
+    mutate(abx_days=replace_na(abx_days,0L)) %>% 
+    mutate(abx_days_cat = cut(abx_days,breaks = c(-1,0,3,5,7,10,14,99), labels = c("0","1-3","4-5","6-7","8-10","11-14",">14"))) 
+  
+  
+  tmp_fit <- lm(duration~age_cat+sex+abx_days_cat, data = tmp_reg_data) 
+  
+  out <- coef(tmp_fit)
+  
+  return(out)
+}
+# run_dur_model3_2(4)
+
+### Run Model 1 ----------------------------------------------------------------
+
+mod_res3 <- distinct(sim_res,trial)
+
+mod_res3 <- mod_res3 %>% 
+  mutate(res = map(trial,run_dur_model3))
+
+### Run Model 2 ----------------------------------------------------------------
+
+mod_res3_2 <- distinct(sim_res,trial)
+
+mod_res3_2 <- mod_res3_2 %>% 
   mutate(res = map(trial,run_dur_model3_2))
 
-mod_res3_2 %>% 
+## Assemble Results ------------------------------------------------------------
+
+mod_res3 <- mod_res3 %>% 
   mutate(res = map(res,enframe)) %>% 
   unnest(res) %>% 
   group_by(name) %>% 
-  summarise(mn_est = mean(value),
+  summarise(mdn_est = median(value),
+            mn_est = mean(value),
             lo_est = quantile(value,probs = c(0.025)),
             hi_est = quantile(value,probs = c(0.975)))
+
+mod_res3_2 <- mod_res3_2 %>% 
+  mutate(res = map(res,enframe)) %>% 
+  unnest(res) %>% 
+  group_by(name) %>% 
+  summarise(mdn_est = median(value),
+            mn_est = mean(value),
+            lo_est = quantile(value,probs = c(0.025)),
+            hi_est = quantile(value,probs = c(0.975)))
+
+
+########################
+#### Export Results ####
+########################
+
+## Save Results ----------------------------------------------------------------
+abx_delay_res <- list(any_abx1 = mod_res1,
+                      any_abx2 = mod_res1_2,
+                      abx_dur1 = mod_res2,
+                      abx_dur2 = mod_res2_2,
+                      abx_cats1 = mod_res3,
+                      abx_cats2 = mod_res3_2)
+
+save(abx_delay_res, file = paste0(out_path, "abx_duration_models.RData"))
+
+## Make Report -----------------------------------------------------------------
+rmarkdown::render(input = "github/delay_diagnosis/build_scripts/R/report_scripts/abx_duration_report.Rmd",
+                  params = list(cond = cond_name),
+                  output_dir = out_path)
+
+
 
 
 
