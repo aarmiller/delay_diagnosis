e88d64002dbcfdee1e531877f63587bf8d6ac6d1
a1arakkal <alan-arakkal@uiowa.edu>
2024-09-05 09:27:57 -0600

    update final_delay_report rmarkdown

--- build_scripts/jobs/final_analysis/atlan/run_job.R
+++ build_scripts/jobs/final_analysis/atlan/run_job.R
@@ -1,2 +1,2 @@
 
-qsub "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/build_scripts/jobs/final_analysis/atlan/run_final_analysis_main.sh" "blasto_NA_state"
+qsub "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/build_scripts/jobs/final_analysis/atlan/run_final_analysis_main.sh" "blasto"
