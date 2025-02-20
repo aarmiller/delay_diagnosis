23f183298acb54a044bc4108078d93f11b41a691
a1arakkal <alan-arakkal@uiowa.edu>
2025-02-20 12:27:35 -0700

    Create make_abx_plots.R

---
+++ build_scripts/R/make_abx_plots.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+source("/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/build_scripts/R/functions/make_abx_plots_fun.R")
+
+conds <- list.dirs("/Shared/Statepi_Diagnosis/prelim_results", recursive = F, full.names = F)
+
+tmp <- parallel::mclapply(conds, 
+                          function(x){make_abx_plots(cond_name = x)},
+                          mc.cores = 30)
