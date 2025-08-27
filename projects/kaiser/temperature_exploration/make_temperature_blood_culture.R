8af4d3f13302a7ed2f5265a81c6add7ef077c3fa
a1arakkal <alan-arakkal@uiowa.edu>
2025-08-27 15:44:40 -0600

    kaiser updates

--- projects/kaiser/temperature_exploration/make_temperature_blood_culture.R
+++ projects/kaiser/temperature_exploration/make_temperature_blood_culture.R
@@ -7,7 +7,7 @@ if(!dir.exists(out_path)){
   dir.create(out_path, recursive = T)
 }
 
-
+set.seed(123) # seed for selecting control
 rmarkdown::render(input = "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/projects/kaiser/temperature_exploration/temperature_blood_culture.Rmd",
                   output_dir = out_path,
                   params = list(cond = cond))
