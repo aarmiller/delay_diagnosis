41ac543082a82dc6f2e03419f21e46e749cc3be0
a1arakkal <alan-arakkal@uiowa.edu>
2025-08-27 13:04:37 -0600

    kaiser updates

---
+++ projects/kaiser/temperature_exploration/archive/make_temperature_blood_culture.R
@@ -0,0 +1,3 @@
+
+rmarkdown::render(input = "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/projects/sepsis_kaiser/temperature_exploration/temperature_blood_culture.Rmd",
+                  output_dir = "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/projects/sepsis_kaiser/temperature_exploration/")
--- projects/sepsis_kaiser/make_temp_explore_kaiser_sepsis.R
+++
@@ -1,5 +0,0 @@
-
-# This script is a placeholder for generating a change-point 
-
-rmarkdown::render(input = "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/projects/sepsis_kaiser/temp_explore_kaiser_sepsis.Rmd",
-                  output_dir = "/Shared/Statepi_Diagnosis/atlan/github/delay_diagnosis/projects/sepsis_kaiser/")
